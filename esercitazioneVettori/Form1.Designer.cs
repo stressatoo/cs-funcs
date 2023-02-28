828f752ad73d344968684628a4679cd1a6b031b9
Alessandro <tryit0utspectre@gmail.com>
2023-02-28 09:49:13 +0100

    Aggiungi ultimo esercizio sui vettori

---
+++ esercitazioneVettori/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace esercitazioneVettori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEsegui = new System.Windows.Forms.Button();
+            this.txtGiorni = new System.Windows.Forms.TextBox();
+            this.txtTotParz_Arance = new System.Windows.Forms.TextBox();
+            this.txtTotParz_Mandarini = new System.Windows.Forms.TextBox();
+            this.txtTotale = new System.Windows.Forms.TextBox();
+            this.txtMaxArance = new System.Windows.Forms.TextBox();
+            this.txtMaxMandarini = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEsegui
+            // 
+            this.btnEsegui.Location = new System.Drawing.Point(203, 119);
+            this.btnEsegui.Name = "btnEsegui";
+            this.btnEsegui.Size = new System.Drawing.Size(75, 23);
+            this.btnEsegui.TabIndex = 0;
+            this.btnEsegui.Text = "Esegui";
+            this.btnEsegui.UseVisualStyleBackColor = true;
+            this.btnEsegui.Click += new System.EventHandler(this.btnEsegui_Click);
+            // 
+            // txtGiorni
+            // 
+            this.txtGiorni.Location = new System.Drawing.Point(121, 38);
+            this.txtGiorni.Name = "txtGiorni";
+            this.txtGiorni.Size = new System.Drawing.Size(100, 20);
+            this.txtGiorni.TabIndex = 1;
+            // 
+            // txtTotParz_Arance
+            // 
+            this.txtTotParz_Arance.Location = new System.Drawing.Point(212, 169);
+            this.txtTotParz_Arance.Name = "txtTotParz_Arance";
+            this.txtTotParz_Arance.Size = new System.Drawing.Size(100, 20);
+            this.txtTotParz_Arance.TabIndex = 2;
+            // 
+            // txtTotParz_Mandarini
+            // 
+            this.txtTotParz_Mandarini.Location = new System.Drawing.Point(212, 195);
+            this.txtTotParz_Mandarini.Name = "txtTotParz_Mandarini";
+            this.txtTotParz_Mandarini.Size = new System.Drawing.Size(100, 20);
+            this.txtTotParz_Mandarini.TabIndex = 3;
+            // 
+            // txtTotale
+            // 
+            this.txtTotale.Location = new System.Drawing.Point(212, 221);
+            this.txtTotale.Name = "txtTotale";
+            this.txtTotale.Size = new System.Drawing.Size(100, 20);
+            this.txtTotale.TabIndex = 4;
+            // 
+            // txtMaxArance
+            // 
+            this.txtMaxArance.Location = new System.Drawing.Point(212, 247);
+            this.txtMaxArance.Name = "txtMaxArance";
+            this.txtMaxArance.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxArance.TabIndex = 5;
+            // 
+            // txtMaxMandarini
+            // 
+            this.txtMaxMandarini.Location = new System.Drawing.Point(212, 273);
+            this.txtMaxMandarini.Name = "txtMaxMandarini";
+            this.txtMaxMandarini.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxMandarini.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 359);
+            this.Controls.Add(this.txtMaxMandarini);
+            this.Controls.Add(this.txtMaxArance);
+            this.Controls.Add(this.txtTotale);
+            this.Controls.Add(this.txtTotParz_Mandarini);
+            this.Controls.Add(this.txtTotParz_Arance);
+            this.Controls.Add(this.txtGiorni);
+            this.Controls.Add(this.btnEsegui);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEsegui;
+        private System.Windows.Forms.TextBox txtGiorni;
+        private System.Windows.Forms.TextBox txtTotParz_Arance;
+        private System.Windows.Forms.TextBox txtTotParz_Mandarini;
+        private System.Windows.Forms.TextBox txtTotale;
+        private System.Windows.Forms.TextBox txtMaxArance;
+        private System.Windows.Forms.TextBox txtMaxMandarini;
+    }
+}
+
---
+++ esercitazioneVettori/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+/*
+In un agrumeto si producono arance e mandarini da destinare al mercato estero. 
+
+Ogni giorno si raccolgono gli agrumi e si inseriscono in due vettori, arance e mandarini, le quantità, in quintali, del raccolto. 
+
+Questa operazione viene fatta per un mese il cui numero di giorni va inserito in input, (gen 31, feb 28 ecc). 
+Si vogliono conoscere i totali dei due vettori, singoli e insieme, il raccolto maggiore per ogni categoria ed il giorno del mese in cui è stato prodotto.
+*/
+
+namespace esercitazioneVettori
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte inserisciGiorni()
+        {
+            byte _giorni;
+
+            _giorni = Convert.ToByte(txtGiorni.Text);
+
+            return _giorni;
+        }
+
+        void input(ref int[] vettore, int contatore, string tipo)
+        {
+            vettore[contatore] = Convert.ToInt32(Interaction.InputBox("Inserire quantità raccolte di " + tipo + ", in quintali, per il giorno numero " + (contatore+1)));
+        }
+
+        void calcolaTotaleParziale(ref int accumulatore, int[] vettore, int contatore)
+        {
+            accumulatore += vettore[contatore];
+        }
+
+        int somma(int numero1, int numero2)
+        {
+            int risultato = numero1 + numero2;
+            return risultato;
+        }
+
+        int calcolaMassimo(int[] vettore)
+        {
+            int massimo;
+            massimo = vettore.Max();
+            return massimo;
+        }
+
+        void output(int cA, int cM, int tA, int mA, int mM)
+        {
+            txtTotParz_Arance.Text = cA.ToString();
+            txtTotParz_Mandarini.Text = cM.ToString();
+            txtTotale.Text = tA.ToString();
+            txtMaxArance.Text = mA.ToString();
+            txtMaxMandarini.Text = mM.ToString();
+        }
+
+        private void btnEsegui_Click(object sender, EventArgs e)
+        {
+            // Giorni (Dimensioni di entrambi i vettori)
+            byte giorni = inserisciGiorni();
+
+            // Vettori
+            int[] arance = new int[giorni];
+            int[] mandarini = new int[giorni];
+
+            // Tipi
+            const string sA = "arance";
+            const string sM = "mandarini";
+
+            // Totali parziali
+            int cArance = 0;
+            int cMandarini = 0;
+
+            // Totale
+            int totaleAgrumi = 0;
+
+            // Massimi
+            int maxArance;
+            int maxMandarini;
+
+            for (int i = 0; i < giorni; ++i)
+            {
+                // Input del contenuto in entrambi i vettori
+                input(ref arance, i, sA);
+                input(ref mandarini, i, sM);
+
+                // Calcolo totali parziali
+                calcolaTotaleParziale(ref cArance, arance, i);
+                calcolaTotaleParziale(ref cMandarini, mandarini, i);
+
+                totaleAgrumi = somma(cArance, cMandarini);
+            }
+
+            maxArance = calcolaMassimo(arance);
+            maxMandarini = calcolaMassimo(mandarini);
+
+            output(cArance, cMandarini, totaleAgrumi, maxArance, maxMandarini);
+        }
+    }
+}
